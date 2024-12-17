74df9a2afde5400644a304f45100f76cdb8be4d6
Kai_Hogan <kaidavishogan@gmail.com>
2024-12-17 10:49:25 -0800

    initial directory

---
+++ Card/fight.py
@@ -0,0 +1,18 @@
+class fight():
+    def __init__(self,name:str,fighter1:str,fighter2:str,fighter1_ml:int,fighter2_ml:int,fighter1_props:dict,fighter2_props:dict,outcome:tuple) -> None:
+        self.name = name
+        self.fighter1 = fighter1
+        self.fighter2 = fighter2
+        self.fighter1_ml = fighter1_ml
+        self.fighter2_ml = fighter2_ml
+        self.fighter1_props = fighter1_props
+        self.fighter2_props = fighter2_props
+        self.outcome = outcome
+    
+    def update_outcome(self,update:tuple):
+        self.outcome = update
+
+    
+
+        
+        
---
+++ User/picks.py
@@ -0,0 +1,11 @@
+from Card import fight
+
+class picks():
+    def __init__(self,picks:list[tuple],remaining_budget:int,payout:int) -> None:
+        self.picks = picks
+        self.remaining_budget = remaining_budget
+        self.payout = payout
+        
+
+        
+        
