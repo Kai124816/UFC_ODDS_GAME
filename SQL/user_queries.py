0dc2afacc61747d2bec5972663b6ebf9a94b694a
Kai_Hogan <kaidavishogan@gmail.com>
2024-12-18 20:25:07 -0800

    set up SQL

---
+++ SQL/user_queries.py
@@ -0,0 +1,37 @@
+import bcrypt 
+import mysql.connector
+from mysql.connector import Error
+
+# Replace these with your local database details
+HOST = "localhost"
+USER = "kai"
+PASSWORD = "hack_24"
+DATABASE = "userdata"
+
+try:
+    # Establish the connection
+    connection = mysql.connector.connect(
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        database=DATABASE
+    )
+
+    if connection.is_connected():
+        print("Connected to MySQL database")
+
+except Error as e:
+    print(f"Error: {e}")
+
+
+
+
+
+
+
+finally:
+    # Close the connection if it was established
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
+        print("Connection closed.")
+
--- User/profile.py
+++ User/profile.py
@@ -1,21 +1,12 @@
 from predictions import picks
 
 class person():
-    def __init__(self,username:str,password:str,history:list[picks],current_picks:picks,total_payout:int,total_profit:int) -> None:
+    def __init__(self,username:str,password:str,picks:picks,total_payout:int,total_profit:int) -> None:
         self.username = username
         self.password = password
-        self.history = history
-        self.current_picks = current_picks
+        self.picks = picks
         self.total_payout = total_payout
         self.total_profit = total_profit
-
-    def commit_to_history(self) -> None:
-        self.history.append(self.current_picks)
-        self.total_payout += self.current_picks.payout
-        self.total_profit += self.current_picks.payout
-        self.total_profit -= 50
-        new_picks = picks([],50,0)
-        self.current_picks = new_picks
     
     def change_password(self,new_password:str) -> None:
         self.password = new_password
